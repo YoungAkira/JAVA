083f376c1e3df8b8cbc7cfcc039a35d67ab9a292
YoungAkira <bielbapt13@egmail.com>
2024-08-28 12:38:08 -0300

    commit initial

---
+++ src/PrintJava.java
@@ -0,0 +1,14 @@
+public class PrintJava {
+
+    public static void main (String[] args) {
+
+        System.out.println("I love pizza");
+        System.out.println("It's really good!");
+        System.out.println();
+        System.out.println();
+
+        System.out.print("\nYOU CAN GET OVER ANYTHING \n");
+        System.out.println("\tYOU TRY!");
+
+    }
+}
