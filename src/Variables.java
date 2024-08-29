2c8fc02a896e4b12b569a2a4af4051dd383777e2
YoungAkira <bielbapt13@gmail.com>
2024-08-29 11:40:14 -0300

    variables

---
+++ src/Variables.java
@@ -0,0 +1,13 @@
+public class Variables {
+    public static void main(String[] args) {
+
+        int x = 123;
+        double y = 3.14;
+        boolean z = true;
+        char symbol = '@';
+        String name = "Bro";
+
+        System.out.print("Hello " +name);
+
+    }
+}
