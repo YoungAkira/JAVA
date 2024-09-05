ea8a29937a56a38e7ca364273c906cb6407ac173
YoungAkira <bielbapt13@gmail.com>
2024-09-05 12:32:06 -0300

    Expression

---
+++ src/Expression.java
@@ -0,0 +1,15 @@
+public class Expression {
+    public static void main(String[] args) {
+
+        int friends = 10;
+
+        int sum = friends ++;
+        int subtraction = friends--;
+        int division = friends / 2;
+
+        System.out.println("addition:  "+sum);
+        System.out.println("subtraction: "+subtraction);
+        System.out.println("division: "+division);
+
+    }
+}
