cdb53d791c62cd71a1d8ca4b6ee06338652a4dcc
YoungAkira <bielbapt13@gmail.com>
2024-09-06 12:27:20 -0300

    Switches

---
+++ src/Switches.java
@@ -0,0 +1,23 @@
+public class Switches {
+
+    public static void main(String[] args) {
+
+        String day = "Friday";
+
+        switch(day) {
+            case "Sunday": System.out.println("It is Sunday!");
+            break;
+            case "Monday": System.out.println("It is Monday!");
+            break;
+            case "Tuesday": System.out.println("It is Tuesday!");
+            break;
+            case "Wednesday": System.out.println("It is Wednesday!");
+            break;
+            case "Thursday": System.out.println("It is Thursday!");
+            break;
+            case "Friday": System.out.println("It is Friday!");
+            break;
+            default:System.out.println("That is not a day");
+        }
+    }
+}
