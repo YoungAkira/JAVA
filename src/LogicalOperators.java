84fde6a4b128f5afbd0d6e596df8c597e742d53b
YoungAkira <bielbapt13@gmail.com>
2024-09-06 14:38:21 -0300

    LogicalOperators

---
+++ src/LogicalOperators.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class LogicalOperators {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("You are playing a game Press q or Q to quit");
+        String response = scanner.next();
+
+
+        if(!response.equals("q")&&!response.equals("Q")) {
+            System.out.println("You are still playing de game!");
+        }
+        else {
+            System.out.println("You quit the game!");
+        }
+    }
+}
