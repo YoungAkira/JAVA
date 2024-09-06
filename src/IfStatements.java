81f7b564ee5daca76a9d441ae7b8a76569b7edbf
YoungAkira <bielbapt13@gmail.com>
2024-09-06 11:35:53 -0300

    IfStatements

---
+++ src/IfStatements.java
@@ -0,0 +1,20 @@
+public class IfStatements {
+
+    public static void main(String[] args) {
+
+        int age = 12;
+
+        if (age>=75) {
+            System.out.println("Ok Boomer!");
+        }
+        else if (age>=18) {
+            System.out.println("You are an adult!");
+        }
+        else if (age>=13) {
+            System.out.println("You are teenager!");
+        }
+        else {
+            System.out.println("You are a kid!");
+        }
+    }
+}
