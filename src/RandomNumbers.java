4f315d59f5dce15ddf05386db4e1a382357d94b8
YoungAkira <bielbapt13@gmail.com>
2024-09-06 10:49:20 -0300

    RandomNumbers

---
+++ src/RandomNumbers.java
@@ -0,0 +1,16 @@
+import java.util.Random;
+
+public class RandomNumbers {
+
+    public static void main(String[] args) {
+
+        Random random = new Random();
+
+        //int x = random.nextInt(10);
+        boolean z = random.nextBoolean();
+
+        System.out.println(z);
+
+    }
+
+}
