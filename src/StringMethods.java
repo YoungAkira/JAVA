f22625a82822828913474d9beb3ce5befeed869a
YoungAkira <bielbapt13@gmail.com>
2024-09-10 11:23:07 -0300

    StringMethods

---
+++ src/StringMethods.java
@@ -0,0 +1,20 @@
+public class StringMethods {
+
+    public static void main(String[] args) {
+
+        String name = "Bro";
+
+        //boolean result = name.equalsIgnoreCase(name);
+        int result = name.length();
+        //char result = name.charAt(0);
+        //int result = name.indexOf("r");
+        //boolean result = name.isEmpty();
+        //String result = name.toUpperCase();
+        //String result = name.toLowerCase();
+        //String result = name.trim();
+        //String result = name.replace("o", "u");
+
+        System.out.println(result);
+
+    }
+}
