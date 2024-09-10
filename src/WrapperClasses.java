362316f6ae283262e2df546be8cdc2cdc34a3cba
YoungAkira <bielbapt13@gmail.com>
2024-09-10 14:04:28 -0300

    WrappedClasses

---
+++ src/WrapperClasses.java
@@ -0,0 +1,15 @@
+public class WrapperClasses {
+
+    public static void main(String[] args) {
+
+        Boolean a = true;
+        Character b = '@';
+        Integer c = 123;
+        Double d = 3.14;
+        String e = "bro";
+
+        if (b=='@') {
+            System.out.println("This is true");
+        }
+    }
+}
