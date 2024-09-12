efdbcc251e3f4edc3d1e9c87b7d195500d9fc086
YoungAkira <bielbapt13@gmail.com>
2024-09-12 11:20:49 -0300

    Method

---
+++ src/Method.java
@@ -0,0 +1,16 @@
+public class Method {
+    
+    public static void main(String[] args) {
+
+        int x = 3;
+        int y = 3;
+
+        System.out.println(add(x, y));
+    }
+
+    static int add(int x, int y) {
+
+        int z = x + y;
+        return z;
+    }
+}
