2abd8e0217bd90d6a2fbe099bf09aca95ffd4f2c
YoungAkira <bielbapt13@gmail.com>
2024-09-12 13:55:53 -0300

    Printf

---
+++ Printf.java
@@ -0,0 +1,14 @@
+public class Printf {
+
+    public static void main(String arg) {
+
+        boolean myBoolean = true;
+        int myInt = 1000;
+        String myString = "Potato";
+        char myChar = '@';
+
+        Printf("this is %b", myBoolean);
+
+    }
+    
+}
